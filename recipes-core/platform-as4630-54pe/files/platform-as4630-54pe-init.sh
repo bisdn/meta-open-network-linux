883b0206ca3742cbaaffc4238168fe4232e81cb9
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2021-11-09 12:13:55 +0100

    accton-as4630-54pe: add platform init code
    Add code and service to initialize the platform. Adapted from https://github.com/opencomputeproject/OpenNetworkLinux/blob/master/packages/platforms/accton/x86-64/as4630-54pe/platform-config/r0/src/python/x86_64_accton_as4630_54pe_r0/__init__.py
    
    Signed-off-by: Rubens Figueiredo <rubens.figueiredo@bisdn.de>

---
+++ recipes-core/platform-as4630-54pe/files/platform-as4630-54pe-init.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -e
+
+create_i2c_dev() {
+  echo $1 $2 > /sys/bus/i2c/devices/i2c-${3}/new_device
+}
+
+add_port() {
+	create_i2c_dev ${1} 0x50 ${3}
+	echo "port${2}" > "/sys/bus/i2c/devices/${3}-0050/port_name"
+}
+
+# load modules
+modprobe x86-64-accton-as4630-54pe-cpld
+modprobe x86-64-accton-as4630-54pe-psu
+modprobe x86-64-accton-as4630-54pe-leds
+
+# init mux (PCA9548)
+create_i2c_dev pca9548 0x77 1
+create_i2c_dev pca9548 0x71 2
+create_i2c_dev pca9548 0x70 3
+
+# add CPLD
+create_i2c_dev as4630_54pe_cpld 0x60 3
+
+# init LM77
+create_i2c_dev lm77 0x48 14
+create_i2c_dev lm75 0x4a 25
+create_i2c_dev lm75 0x4b 24
+
+# init PSU-1
+create_i2c_dev as4630_54pe_psu1 0x50 10
+create_i2c_dev ype1200am 0x58 10
+
+# init PSU-2
+create_i2c_dev as4630_54pe_psu2 0x51 11
+create_i2c_dev ype1200am 0x59 11
+
+for port in {49..52}; do
+	add_port 'optoe2' $port $((port-31))
+done
+
+for port in {53..54}; do
+	add_port 'optoe1' $port $((port-31))
+done
+
+# add EEPROM
+create_i2c_dev 24c02 0x57 1
