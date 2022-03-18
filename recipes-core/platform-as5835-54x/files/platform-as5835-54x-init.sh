9c8d7f4af6a9658fe52078eee482df15d9c6edc9
Roger Luethi <roger.luethi@bisdn.de>
2022-03-18 14:08:49 +0100

    recipes-core: make create_i2c_dev on as5835-54x more robust
    This commit adds two features to the platform init code (inspired
    by platform-as7726-32x-init.sh):
    
    - make sure the i2c bus file is present before trying to write it; we
      could also do this just once instead of for every call, but this is
      robust and should be fast enough
    
    - for the pca9548 multiplexer, set idle_state to MUX_IDLE_DISCONNECT
      (-2). Not doing this may result in errors, for instance SFP eeproms
      being read incorrectly. On the 5835-54X, "onlpdump -S" labeled
      100 G cables as "UNK" (unknown) and "onlpdump" returned incorrect
      eeprom dumps for them and printed to stderr the error message
      "[sff] sff_info_valid() failed: invalid module type".
    
      The idea can also be found in the sonic code for this switch [1].
    
    [1] https://github.com/Azure/sonic-buildimage/commit/85976cbca3a59c94a14802e0e6c416d75b4bb924
    
    Signed-off-by: Roger Luethi <roger.luethi@bisdn.de>

--- recipes-core/platform-as5835-54x/files/platform-as5835-54x-init.sh
+++ recipes-core/platform-as5835-54x/files/platform-as5835-54x-init.sh
@@ -8,8 +8,33 @@
 
 set -o errexit -o nounset
 
+function wait_for_file() {
+  FILE=$1
+  i=0
+  while [ $i -lt 10 ]; do
+    test -e $FILE && return 0
+    i=$(( i + 1 ))
+    sleep 1
+  done
+  return 1
+}
+
 create_i2c_dev() {
-  echo $1 $2 > /sys/bus/i2c/devices/i2c-${3}/new_device
+  local i2c_dev_name=$1
+  local i2c_dev_addr=$2
+  local i2c_bus_no=$3
+
+  wait_for_file "/sys/bus/i2c/devices/i2c-$i2c_bus_no/new_device"
+  echo "$i2c_dev_name" "$i2c_dev_addr" > /sys/bus/i2c/devices/i2c-${i2c_bus_no}/new_device
+
+  # For pca9548 multiplexer, set idle_state to -2 (MUX_IDLE_DISCONNECT)
+  # Not doing this can result, for instance, in SFP EEPROMs not being
+  # read correctly by onlp.
+  if [ "$i2c_dev_name" = "pca9548" ]; then
+    # Replace leading "0x" with "00"
+    i2c_dev_addr_4="00${i2c_dev_addr:2}"
+    echo '-2' > /sys/bus/i2c/devices/$i2c_bus_no-$i2c_dev_addr_4/idle_state
+  fi
 }
 
 # load modules
