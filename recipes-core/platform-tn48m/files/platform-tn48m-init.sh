7694e2a3970057c4ed7b0e79bbe96da409d79b7b
Roger Luethi <roger.luethi@bisdn.de>
2022-03-30 11:12:46 +0200

    core: tn48m: add platform-tn48m recipe and related files
    Copy platform recipe and init files for the delta-tn48m from
    platform-as4610 (change platform name in file names and content) and
    have the platform depend on it.
    
    Platform init.sh based on accton-as5835-54x, creating i2c device nodes
    based on information from:
    
    https://github.com/dentproject/dentOS/blob/fa38697b85485ba909b0c9c4f8e5e48bfddbd8fd/packages/platforms/delta/arm64/tn48m/tn48m/platform-config/r0/src/python/arm64_delta_tn48m_r0/__init__.py
    
    https://github.com/dentproject/dentOS/blob/fa38697b85485ba909b0c9c4f8e5e48bfddbd8fd/packages/platforms/delta/arm64/tn48m-dn/tn48m-dn/platform-config/r0/src/python/arm64_delta_tn48m_dn_r0/__init__.py
    
    Signed-off-by: Roger Luethi <roger.luethi@bisdn.de>

---
+++ recipes-core/platform-tn48m/files/platform-tn48m-init.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# Platform init for tn48m
+#
+# XXX We might be able to learn something from
+# https://github.com/dentproject/dentOS/blob/fa38697b85485ba909b0c9c4f8e5e48bfddbd8fd/packages/platforms/delta/arm64/tn48m/tn48m/platform-config/r0/src/python/arm64_delta_tn48m_r0/__init__.py
+
+set -o errexit -o nounset
+
+function wait_for_file() {
+  FILE=$1
+  i=0
+  while [ $i -lt 10 ]; do
+    test -e $FILE && return 0
+    i=$(( i + 1 ))
+    sleep 1
+  done
+  return 1
+}
+
+create_i2c_dev() {
+  local i2c_dev_name=$1
+  local i2c_dev_addr=$2
+  local i2c_bus_no=$3
+
+  wait_for_file "/sys/bus/i2c/devices/i2c-$i2c_bus_no/new_device"
+  echo "$i2c_dev_name" "$i2c_dev_addr" > /sys/bus/i2c/devices/i2c-${i2c_bus_no}/new_device
+
+  # For pca9548 multiplexer, set idle_state to -2 (MUX_IDLE_DISCONNECT)
+  # Not doing this can result, for instance, in SFP EEPROMs not being
+  # read correctly by onlp.
+  if [ "$i2c_dev_name" = "pca9548" ]; then
+    # Replace leading "0x" with "00"
+    i2c_dev_addr_4="00${i2c_dev_addr:2}"
+    echo '-2' > /sys/bus/i2c/devices/$i2c_bus_no-$i2c_dev_addr_4/idle_state
+  fi
+}
+
+# load modules
+modprobe arm64-delta-tn48m-cpld
+modprobe arm64-delta-tn48m-led
+modprobe prestera_pci
+
+# fan controller
+create_i2c_dev adt7473 0x2e 1
+
+# temperature devices
+create_i2c_dev tmp1075 0x4a 1
+create_i2c_dev tmp1075 0x4b 1
