2ef5d8aac8c9f2b232be10635bbdf807d158e66d
Jonas Gorski <jonas.gorski@bisdn.de>
2022-11-21 10:19:47 +0100

    onl: move questone-2a support into a patch
    Instead of awkwardly copying the questone-2a support into the source
    tree add it via a patch to (somewhat) keep the original authorship
    intact.
    
    To allow this, we need to patch infra to not try to load the file copies
    created by quilt when applying patches.
    
    Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

--- recipes-extended/onl/files/questone-2a/onlp/builds/x86_64_cel_questone_2a/module/auto/make.mk
+++
@@ -1,9 +0,0 @@
-###############################################################################
-#
-# x86_64_cel_questone_2a Autogeneration
-#
-###############################################################################
-x86_64_cel_questone_2a_AUTO_DEFS := module/auto/x86_64_cel_questone_2a.yml
-x86_64_cel_questone_2a_AUTO_DIRS := module/inc/x86_64_cel_questone_2a module/src
-include $(BUILDER)/auto.mk
-
