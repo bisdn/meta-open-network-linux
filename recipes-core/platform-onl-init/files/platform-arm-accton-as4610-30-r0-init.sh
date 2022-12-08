590bedd1d0e728e8d21e89365ed196ee41e10c5e
Jonas Gorski <jonas.gorski@bisdn.de>
2022-12-08 11:25:18 +0100

    platform-onl-init: accton-as4610-30/54: use distinct init code
    Replace the symlink of as4610-54 to as4610-30 with its own copy, and
    drop the platform check, since we know which platform we are already via
    the executed script.
    
    Simplifies the code significantly.
    
    Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

--- recipes-core/platform-onl-init/files/platform-arm-accton-as4610-30-r0-init.sh
+++ recipes-core/platform-onl-init/files/platform-arm-accton-as4610-30-r0-init.sh
@@ -1,32 +1,12 @@
 #!/bin/bash
 
-create_i2c_dev() {
-	local product_id="$(cat /sys/bus/i2c/drivers/as4610_54_cpld/0-0030/product_id)"
-	case "$product_id" in
-		"0" | "1")
-			echo port25 > /sys/bus/i2c/devices/2-0050/port_name
-			echo port26 > /sys/bus/i2c/devices/3-0050/port_name
-			echo port27 > /sys/bus/i2c/devices/4-0050/port_name
-			echo port28 > /sys/bus/i2c/devices/5-0050/port_name
-			echo port29 > /sys/bus/i2c/devices/6-0050/port_name
-			echo port30 > /sys/bus/i2c/devices/7-0050/port_name
-			;;
-		"2" | "3" | "5")
-			echo port49 > /sys/bus/i2c/devices/2-0050/port_name
-			echo port50 > /sys/bus/i2c/devices/3-0050/port_name
-			echo port51 > /sys/bus/i2c/devices/4-0050/port_name
-			echo port52 > /sys/bus/i2c/devices/5-0050/port_name
-			echo port53 > /sys/bus/i2c/devices/6-0050/port_name
-			echo port54 > /sys/bus/i2c/devices/7-0050/port_name
-			;;
-		*)
-			echo "platform-as4610-init: unknown product id $product_id"
-			return
-			;;
-	esac
+echo port25 > /sys/bus/i2c/devices/2-0050/port_name
+echo port26 > /sys/bus/i2c/devices/3-0050/port_name
+echo port27 > /sys/bus/i2c/devices/4-0050/port_name
+echo port28 > /sys/bus/i2c/devices/5-0050/port_name
+echo port29 > /sys/bus/i2c/devices/6-0050/port_name
+echo port30 > /sys/bus/i2c/devices/7-0050/port_name
 
-	# take out PHYs from reset
-	i2cset -y -f 0 0x30 0x19 0
-	i2cset -y -f 0 0x30 0x2a 0
-}
-create_i2c_dev
+# take out PHYs from reset
+i2cset -y -f 0 0x30 0x19 0
+i2cset -y -f 0 0x30 0x2a 0
