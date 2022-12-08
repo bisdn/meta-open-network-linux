72ee113989d7ed094ddf9a42e2c4c992cc3dd7f2
Jonas Gorski <jonas.gorski@bisdn.de>
2022-12-08 11:25:18 +0100

    platform-onl-init: accton-as4610-30/54: use distinct init code
    Replace the symlink of as4610-54 to as4610-30 with its own copy, and
    drop the platform check, since we know which platform we are already via
    the executed script.
    
    Simplifies the code significantly.
    
    Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

--- recipes-core/platform-onl-init/files/platform-arm-accton-as4610-54-r0-init.sh
+++ recipes-core/platform-onl-init/files/platform-arm-accton-as4610-54-r0-init.sh
@@ -1,1 +1,12 @@
-platform-arm-accton-as4610-30-r0-init.sh
+#!/bin/bash
+
+echo port49 > /sys/bus/i2c/devices/2-0050/port_name
+echo port50 > /sys/bus/i2c/devices/3-0050/port_name
+echo port51 > /sys/bus/i2c/devices/4-0050/port_name
+echo port52 > /sys/bus/i2c/devices/5-0050/port_name
+echo port53 > /sys/bus/i2c/devices/6-0050/port_name
+echo port54 > /sys/bus/i2c/devices/7-0050/port_name
+
+# take out PHYs from reset
+i2cset -y -f 0 0x30 0x19 0
+i2cset -y -f 0 0x30 0x2a 0
