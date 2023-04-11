c58b58791bb57d5b37fd93fd62148f7a08725330
Roger Luethi <roger.luethi@bisdn.de>
2023-04-11 14:09:57 +0200

    EXP platform-x86-64-accton-csp7551-r0-init.sh: use generic functions
    Signed-off-by: Roger Luethi <roger.luethi@bisdn.de>

--- recipes-core/platform-onl-init/files/platform-x86-64-accton-csp7551-r0-init.sh
+++ recipes-core/platform-onl-init/files/platform-x86-64-accton-csp7551-r0-init.sh
@@ -4,26 +4,6 @@
 
 set -o errexit -o nounset
 
-function wait_for_file() {
-  FILE=$1
-  i=0
-  while [ $i -lt 10 ]; do
-    test -e $FILE && return 0
-    i=$(( i + 1 ))
-    sleep 1
-  done
-  return 1
-}
-
-create_i2c_dev() {
-  local i2c_dev_name=$1
-  local i2c_dev_addr=$2
-  local i2c_bus_no=$3
-
-  wait_for_file "/sys/bus/i2c/devices/i2c-$i2c_bus_no/new_device"
-  echo "$i2c_dev_name" "$i2c_dev_addr" > /sys/bus/i2c/devices/i2c-${i2c_bus_no}/new_device
-}
-
 modprobe -r i2c-i801 || true
 modprobe fpga_driver
 modprobe accton_i2c_cpld
