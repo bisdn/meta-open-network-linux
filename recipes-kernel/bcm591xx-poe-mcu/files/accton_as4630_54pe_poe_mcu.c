76012f2ebdd93b69baa1039b512172b00ee9aec0
Jonas Gorski <jonas.gorski@bisdn.de>
2023-04-19 15:34:44 +0000

    bcm591xx-poe-mcu: as4630-54pe: switch to common message code
    Make use of the newly added common message code, and replace the send
    function with a simplified version.
    
    Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

--- recipes-kernel/bcm591xx-poe-mcu/files/accton_as4630_54pe_poe_mcu.c
+++ recipes-kernel/bcm591xx-poe-mcu/files/accton_as4630_54pe_poe_mcu.c
@@ -74,81 +74,42 @@ struct as4630_poe_port {
 };
 
 struct as4630_poe_pse {
+	struct bcm591xx_pse_mcu mcu;
 	struct i2c_client *client;
 	struct dentry *debugfs;
 
 	struct mutex mutex;
-
-	u8 tx_counter;
 
 	int num_ports;
 
 	struct as4630_poe_port *ports;
 };
 
-static int as4630_poe_pse_csum(const void *data)
+#define to_as4630_poe_pse(d)    container_of(d, struct as4630_poe_pse, mcu)
+
+static int as4630_poe_pse_do_txrx(struct bcm591xx_pse_mcu *mcu,
+				  struct pse_msg *cmd, struct pse_msg *resp)
 {
-	const u8 *tmp = data;
-	int i;
-	u8 csum = 0;
-
-	for (i = 0; i < sizeof(struct pse_msg) - 1; i++)
-		csum += *tmp++;
-
-	return csum;
-}
-
-static int as4630_poe_pse_send(struct as4630_poe_pse *pse, struct pse_msg *cmd,
-			       struct pse_msg *resp, int counter)
-{
+	struct as4630_poe_pse *pse = to_as4630_poe_pse(mcu);
 	struct i2c_client *client = pse->client;
-	int csum, ret = 0;
 	struct pse_msg tmp;
-
-	if (counter >= 0) {
-		cmd->counter = counter;
-	} else {
-		cmd->counter = pse->tx_counter++;
-		if (pse->tx_counter == 0xfe)
-			pse->tx_counter = 0x01;
-	}
-
-	cmd->csum = as4630_poe_pse_csum(cmd);
-
-	dev_dbg(&client->dev, "sending message: op=%02x cnt=%02x data=%*ph\n",
-		cmd->opcode, cmd->counter, sizeof(cmd->data), cmd->data);
+	int ret = 0;
 
 	ret = i2c_smbus_write_i2c_block_data(client, cmd->opcode,
 					     sizeof(*cmd) - 1, &cmd->counter);
-	if (ret < 0) {
-		dev_info(&client->dev, "failed writing message: %i\n", ret);
-		goto out;
-	}
+	if (ret < 0)
+		return ret;
 
 	msleep(40);
 
-	ret = i2c_smbus_read_i2c_block_data(client, 0, sizeof(tmp),
-					    (u8* )&tmp);
-	if (ret < 0) {
-		/* MCU_OP_PSE_RESET may not yield a response */
-		if (cmd->opcode != MCU_OP_PSE_RESET) {
-			dev_err(&pse->client->dev, "time out while waiting for response\n");
-			ret = -ETIMEDOUT;
-		}
-		goto out;
-	}
-
-	dev_dbg(&client->dev, "received message: op=%02x cnt=%02x data=%*ph [csum=%02x %s]\n",
-		tmp.opcode, tmp.counter, sizeof(tmp.data), tmp.data, tmp.csum,
-		tmp.csum == as4630_poe_pse_csum(&tmp) ? "ok" : "ng");
-
-	if (tmp.csum != as4630_poe_pse_csum(&tmp))
-		ret = -EIO;
+	ret = i2c_smbus_read_i2c_block_data(client, 0, sizeof(tmp), (u8* )&tmp);
+	if (ret < 0)
+		return ret;
 
 	if (resp)
 		memcpy(resp, &tmp, sizeof(tmp));
-out:
-	return ret < 0 ? ret : 0;
+
+	return 0;
 }
 
 static int as4630_poe_pse_enable(struct as4630_poe_pse *pse,
@@ -164,7 +125,7 @@ static int as4630_poe_pse_enable(struct as4630_poe_pse *pse,
 	cmd.data[1] = enable;
 
 	mutex_lock(&pse->mutex);
-	ret = as4630_poe_pse_send(pse, &cmd, NULL, COUNTER_AUTO);
+	ret = bcm591xx_send(&pse->mcu, &cmd, NULL, COUNTER_AUTO);
 	mutex_unlock(&pse->mutex);
 
 	if (!ret)
@@ -188,7 +149,7 @@ static int as4630_poe_pse_init_ports(struct as4630_poe_pse *pse,
 	cmd.data[7] = PSE_EN_DEFAULT;
 	cmd.data[8] = 0xff;
 
-	return as4630_poe_pse_send(pse, &cmd, NULL, COUNTER_AUTO);
+	return bcm591xx_send(&pse->mcu, &cmd, NULL, COUNTER_AUTO);
 }
 
 static int as4630_poe_pse_init(struct as4630_poe_pse *pse)
@@ -301,7 +262,7 @@ static ssize_t read_file_port_status(struct file *file, char __user *user_buf,
 	cmd.opcode = MCU_OP_PSE_PORT_STATUS;
 	cmd.data[0] = port->port_num;
 
-	ret = as4630_poe_pse_send(pse, &cmd, &resp, COUNTER_AUTO);
+	ret = bcm591xx_send(&pse->mcu, &cmd, &resp, COUNTER_AUTO);
 	mutex_unlock(&pse->mutex);
 	if (ret)
 		return ret;
@@ -333,7 +294,7 @@ static ssize_t read_file_port_measurement(struct file *file, char __user *user_b
 	cmd.opcode = MCU_OP_PSE_PORT_MEASUREMENT;
 	cmd.data[0] = port->port_num;
 
-	ret = as4630_poe_pse_send(pse, &cmd, &resp, COUNTER_AUTO);
+	ret = bcm591xx_send(&pse->mcu, &cmd, &resp, COUNTER_AUTO);
 	mutex_unlock(&pse->mutex);
 	if (ret)
 		return ret;
@@ -372,7 +333,7 @@ static ssize_t read_file_status(struct file *file, char __user *user_buf,
 	memset(cmd.data, 0xff, sizeof(cmd.data));
 	cmd.opcode = MCU_OP_PSE_STATUS;
 
-	ret = as4630_poe_pse_send(pse, &cmd, &resp, COUNTER_AUTO);
+	ret = bcm591xx_send(&pse->mcu, &cmd, &resp, COUNTER_AUTO);
 	mutex_unlock(&pse->mutex);
 	if (ret)
 		return ret;
@@ -413,6 +374,10 @@ static void as4630_poe_pse_debugfs_create(struct as4630_poe_pse *pse)
 	}
 }
 
+static const struct bcm591xx_ops as4630_poe_pse_ops = {
+	.do_txrx = as4630_poe_pse_do_txrx,
+};
+
 static int as4630_poe_pse_probe(struct i2c_client *client, const struct i2c_device_id *dev_id)
 {
 	int num_ports = 0, ret, reg, psu_rating;
@@ -430,11 +395,13 @@ static int as4630_poe_pse_probe(struct i2c_client *client, const struct i2c_devi
 	mutex_init(&pse->mutex);
 
 	pse->client = client;
-	pse->tx_counter = 1;
+	pse->mcu.dev = &client->dev;
+	pse->mcu.tx_counter = 1;
+	pse->mcu.ops = &as4630_poe_pse_ops,
 
 	memset(cmd.data, 0xff, sizeof(cmd.data));
 	cmd.opcode = MCU_OP_PSE_STATUS;
-	ret = as4630_poe_pse_send(pse, &cmd, &resp, 0);
+	ret = bcm591xx_send(&pse->mcu, &cmd, &resp, 0);
 	if (ret)
 		return ret;
 
