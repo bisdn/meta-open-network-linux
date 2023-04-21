591eb0ac144880ce63c81a9f335799a6944e9d88
Jonas Gorski <jonas.gorski@bisdn.de>
2023-04-21 08:52:38 +0000

    bcm591xx-poe-mcu: add debugfs code to common module
    Now that the common struct contains all required information, we can
    create shared debugfs code in the common module.
    
    Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

--- recipes-kernel/bcm591xx-poe-mcu/files/bcm591xx.c
+++ recipes-kernel/bcm591xx-poe-mcu/files/bcm591xx.c
@@ -5,9 +5,13 @@
  * Author: Jonas Gorski <jonas.gorski@bisdn.de>
  */
 
+#include <linux/debugfs.h>
 #include <linux/device.h>
 #include <linux/export.h>
+#include <linux/fs.h>
 #include <linux/module.h>
+
+#include <asm/unaligned.h>
 
 #include "bcm591xx.h"
 
@@ -67,6 +71,200 @@ int bcm591xx_send(struct bcm591xx_pse_mcu *mcu, struct pse_msg *cmd,
 	return 0;
 }
 EXPORT_SYMBOL_GPL(bcm591xx_send);
+
+static int bcm591xx_port_enable(struct bcm591xx_pse_mcu *mcu,
+				struct bcm591xx_port *port, bool enable)
+{
+	struct pse_msg cmd;
+	int ret;
+
+	memset(cmd.data, 0xff, sizeof(cmd.data));
+
+	cmd.opcode = MCU_OP_PSE_EN;
+	cmd.data[0] = port->port_num;
+	cmd.data[1] = enable;
+
+	mutex_lock(&mcu->mutex);
+	ret = bcm591xx_send(mcu, &cmd, NULL, COUNTER_AUTO);
+	mutex_unlock(&mcu->mutex);
+
+	if (!ret)
+		port->pse_en = enable;
+	return ret;
+}
+
+static ssize_t read_file_port_enable(struct file *file, char __user *user_buf,
+				     size_t count, loff_t *ppos)
+{
+	struct bcm591xx_port *port = file->private_data;
+	unsigned int len;
+	char buf[32];
+
+	len = sprintf(buf, "%d\n", port->pse_en);
+
+	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
+}
+
+static ssize_t write_file_port_enable(struct file *file, const char __user *user_buf,
+				      size_t count, loff_t *ppos)
+{
+	struct bcm591xx_port *port = file->private_data;
+	unsigned long enable;
+	unsigned int len;
+	char buf[32];
+
+	len = min(count, sizeof(buf) - 1);
+	if (copy_from_user(buf, user_buf, len))
+		return -EFAULT;
+
+	buf[len] = '\0';
+	if (kstrtoul(buf, 0, &enable))
+		return -EINVAL;
+
+	if (enable != 0 && enable != 1)
+		return -EINVAL;
+
+	if (port->pse_en != enable)
+		bcm591xx_port_enable(port->parent, port, enable);
+
+	return count;
+}
+
+static const struct file_operations bcm591xx_port_en_ops = {
+	.read = read_file_port_enable,
+	.write = write_file_port_enable,
+	.open = simple_open,
+	.owner = THIS_MODULE,
+	.llseek = default_llseek,
+};
+
+static ssize_t read_file_port_status(struct file *file, char __user *user_buf,
+				     size_t count, loff_t *ppos)
+{
+	struct bcm591xx_port *port = file->private_data;
+	struct bcm591xx_pse_mcu *mcu = port->parent;
+	struct pse_msg cmd, resp;
+	unsigned int len;
+	char buf[32];
+	int ret;
+
+	mutex_lock(&mcu->mutex);
+	memset(cmd.data, 0xff, sizeof(cmd.data));
+	cmd.opcode = MCU_OP_PSE_PORT_STATUS;
+	cmd.data[0] = port->port_num;
+
+	ret = bcm591xx_send(mcu, &cmd, &resp, COUNTER_AUTO);
+	mutex_unlock(&mcu->mutex);
+	if (ret)
+		return ret;
+
+	len = sprintf(buf, "%*ph\n", sizeof(resp.data), resp.data);
+
+	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
+}
+
+static const struct file_operations bcm591xx_port_status_ops = {
+	.read = read_file_port_status,
+	.open = simple_open,
+	.owner = THIS_MODULE,
+	.llseek = default_llseek,
+};
+
+static ssize_t read_file_port_measurement(struct file *file, char __user *user_buf,
+				          size_t count, loff_t *ppos)
+{
+	struct bcm591xx_port *port = file->private_data;
+	struct bcm591xx_pse_mcu *mcu = port->parent;
+	struct pse_msg cmd, resp;
+	char buf[64];
+	unsigned int len;
+	int ret, voltage, curr, temp, power;
+
+	mutex_lock(&mcu->mutex);
+	memset(cmd.data, 0xff, sizeof(cmd.data));
+	cmd.opcode = MCU_OP_PSE_PORT_MEASUREMENT;
+	cmd.data[0] = port->port_num;
+
+	ret = bcm591xx_send(mcu, &cmd, &resp, COUNTER_AUTO);
+	mutex_unlock(&mcu->mutex);
+	if (ret)
+		return ret;
+
+	voltage = DIV_ROUND_CLOSEST(get_unaligned_be16(&resp.data[1]) * 6445, 1000);
+	curr = get_unaligned_be16(&resp.data[3]);
+	temp = ((get_unaligned_be16(&resp.data[5]) - 120) * -125) + 12500;
+	power = get_unaligned_be16(&resp.data[7]);
+
+	len = sprintf(buf, "%i.%02iV\n%i.%03iA\n%i.%02iC\n%i.%01iW\n",
+		      voltage / 100, voltage % 100,
+		      curr / 1000, curr % 1000,
+		      temp / 100, temp % 100,
+		      power / 10, power % 10);
+
+	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
+}
+
+static const struct file_operations bcm591xx_port_measurement_ops = {
+	.read = read_file_port_measurement,
+	.open = simple_open,
+	.owner = THIS_MODULE,
+	.llseek = default_llseek,
+};
+
+static ssize_t read_file_status(struct file *file, char __user *user_buf,
+				size_t count, loff_t *ppos)
+{
+	struct bcm591xx_pse_mcu *mcu = file->private_data;
+	struct pse_msg cmd, resp;
+	unsigned int len;
+	char buf[32];
+	int ret;
+
+	mutex_lock(&mcu->mutex);
+	memset(cmd.data, 0xff, sizeof(cmd.data));
+	cmd.opcode = MCU_OP_PSE_STATUS;
+
+	ret = bcm591xx_send(mcu, &cmd, &resp, COUNTER_AUTO);
+	mutex_unlock(&mcu->mutex);
+	if (ret)
+		return ret;
+
+	len = sprintf(buf, "%*ph\n", sizeof(resp.data), resp.data);
+
+	return simple_read_from_buffer(user_buf, count, ppos, buf, len);
+}
+
+static const struct file_operations bcm591xx_status_ops = {
+	.read = read_file_status,
+	.open = simple_open,
+	.owner = THIS_MODULE,
+	.llseek = default_llseek,
+};
+
+void bcm591xx_debugfs_create(struct bcm591xx_pse_mcu *mcu)
+{
+	int i;
+
+	mcu->debugfs = debugfs_create_dir(dev_name(mcu->dev), NULL);
+	debugfs_create_file("status", 0200, mcu->debugfs, mcu, &bcm591xx_status_ops);
+
+	for (i = 0; i < mcu->num_ports; i++) {
+		struct dentry *portdir;
+		char dirname[16];
+
+		sprintf(dirname, "port%d", i);
+
+		portdir = debugfs_create_dir(dirname, mcu->debugfs);
+
+		if (!portdir)
+			return;
+
+		debugfs_create_file("enable", 0600, portdir, &mcu->ports[i], &bcm591xx_port_en_ops);
+		debugfs_create_file("status", 0200, portdir, &mcu->ports[i], &bcm591xx_port_status_ops);
+		debugfs_create_file("measurement", 0200, portdir, &mcu->ports[i], &bcm591xx_port_measurement_ops);
+	}
+}
+EXPORT_SYMBOL_GPL(bcm591xx_debugfs_create);
 
 MODULE_AUTHOR("Jonas Gorski <jonas.gorski@bisdn.de>");
 MODULE_DESCRIPTION("BCM591XX MCU library functions");
